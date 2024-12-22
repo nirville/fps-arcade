813fef4377be81d6169b01d0b7a412d4efe0331d
Niraj Sharma <nirajhimself@gmail.com>
2024-12-23 01:36:25 +0530

    added log test

--- Assets/_Assets/Scripts/Weapon.cs
+++ Assets/_Assets/Scripts/Weapon.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Weapon : MonoBehaviour
 {
     void OnCollisionEnter(Collision other) {
+        Debug.Log("Collided with " + other.gameObject.name);
         gameObject.GetComponent<Rigidbody>().isKinematic = true;
         gameObject.GetComponent<Rotator>().shouldRotate = false;
     }
